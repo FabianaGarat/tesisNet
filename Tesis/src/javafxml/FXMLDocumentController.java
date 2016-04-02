876deacdd52af32f8b87130e3c71611ed5d43e7f
FabianaGarat <garatfabiana_89@hotmail.com>
2016-04-02 17:40:40 -0300

    REECUPEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

---
+++ Tesis/src/javafxml/FXMLDocumentController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxml;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Fabiana
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML private TextField idNombre, idApellido, idDatos;
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    @FXML
+     private void acctionMostrar(ActionEvent event) {
+        idDatos.setText("Nombre "+idNombre.getText());
+    }
+    
+}
